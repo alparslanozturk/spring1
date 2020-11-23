917a0636c95a8e44b7d316cd1b1ef7aa42942c57
root <root@ornek.com>
2020-11-23 19:51:08 +0300

    home ve merhaba dünya eklendi.

--- initial/src/main/java/hello/Application.java
+++ initial/src/main/java/hello/Application.java
@@ -2,9 +2,17 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Application {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Merhaba Docker Dünayasii...";
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
